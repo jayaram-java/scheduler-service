2dbc49f7777b892bd00fbcf1a173e35fbd10c6d3
jayaram-java <jayaramp51096@gmail.com>
2026-02-28 17:18:45 +0530

    Eureka client

--- src/main/java/com/bank/scheduler/scheduler_service/SchedulerServiceApplication.java
+++ src/main/java/com/bank/scheduler/scheduler_service/SchedulerServiceApplication.java
@@ -2,10 +2,12 @@ package com.bank.scheduler.scheduler_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class SchedulerServiceApplication {
 
 	public static void main(String[] args) {
